a87e4fce330d8a7aef45bd7a6f20af02227321e7
AlejandroGarciaBautista <alu0101387604@ull.edu.es>
2025-03-23 15:38:14 +0000

    Topology Script nearly finished

--- Scripts_mininet/topology.py
+++ Scripts_mininet/topology.py
@@ -11,14 +11,16 @@ def parse_arguments():
     parser.add_argument("--spine", type=int, default=2, help="Cantidad de switches spine (por defecto: 2)")
     parser.add_argument("--leaf", type=int, default=4, help="Cantidad de switches leaf (por defecto: 4)")
     parser.add_argument("--hosts", type=int, default=2, help="Cantidad de hosts por switch leaf (por defecto: 2)")
-    parser.add_argument("--bw", type=int, default=20, help="Capacidad de los enlaces en Mbps (por defecto: 1000)")
+    parser.add_argument("--bw", type=int, default=100, help="Capacidad de los enlaces en Mbps (por defecto: 100)")
+    parser.add_argument("--rd", type=bool, default=False, help="Aplicar Redundancia en la red (por defecto: False)")
+    parser.add_argument("-c", type=str, default="127.0.0.1", help="Dirección IP del controlador SDN (por defecto: 127.0.0.1)")
 
     return parser.parse_args()
 
-def create_spine_leaf_topology(spine_switches, leaf_switches, hosts_per_leaf, link_bandwidth):
+def create_spine_leaf_topology(spine_switches, leaf_switches, hosts_per_leaf, link_bandwidth, redundancy, controller_ip):
     net = Mininet(controller=None, switch=OVSSwitch, link=TCLink)
     # Agregar el controlador remoto
-    c0 = net.addController('c0', controller=RemoteController, ip='127.0.0.1', port=6633, protocols="OpenFlow13")
+    c0 = net.addController('c0', controller=RemoteController, ip=controller_ip, port=6633, protocols="OpenFlow13")
 
     # Crear switches spine y leaf con nombres más claros
     spines = [net.addSwitch(f"spine{i+1}") for i in range(spine_switches)]
@@ -27,10 +29,8 @@ def create_spine_leaf_topology(spine_switches, leaf_switches, hosts_per_leaf, li
     # Conectar switches leaf a los switches spine con redundancia
     for leaf in leaves:
         for spine in spines:
-            # net.addLink(leaf, spine, bw=link_bandwidth, htb=True, r2q=r2q_value)
-            # net.addLink(spine, leaf, bw=link_bandwidth, htb=True, r2q=r2q_value)
             net.addLink(leaf, spine, cls=TCLink, bw=link_bandwidth, htb=True)
-            #net.addLink(spine, leaf)
+            if (redundancy): net.addLink(leaf, spine, cls=TCLink, bw=link_bandwidth, htb=True)
 
     # Agregar hosts y conectarlos a los switches leaf
     host_count = 1
@@ -38,7 +38,6 @@ def create_spine_leaf_topology(spine_switches, leaf_switches, hosts_per_leaf, li
         for _ in range(hosts_per_leaf):
             host = net.addHost(f"h{host_count}")
             net.addLink(host, leaf, cls=TCLink, bw=link_bandwidth, htb=True)
-            # net.addLink(host, leaf, bw=link_bandwidth, htb=True, r2q=r2q_value)
 
             host_count += 1
 
@@ -54,4 +53,4 @@ def create_spine_leaf_topology(spine_switches, leaf_switches, hosts_per_leaf, li
 if __name__ == "__main__":
     setLogLevel("info")
     args = parse_arguments()
-    create_spine_leaf_topology(args.spine, args.leaf, args.hosts, args.bw)
+    create_spine_leaf_topology(args.spine, args.leaf, args.hosts, args.bw, args.rd, args.c)
