25b2c545981c3b2db5c455eea487d85a627cee26
AlejandroGarciaBautista <alu0101387604@ull.edu.es>
2025-03-30 12:26:16 +0000

    Testing NetworkX

---
+++ Scripts_Ryu/NetworkX.py
@@ -0,0 +1,96 @@
+from ryu.base import app_manager
+from ryu.controller import event, ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.lib import hub
+from ryu.topology import event as topo_event
+from ryu.topology.api import get_switch, get_link, get_host
+import networkx as nx
+from ryu.lib.packet import packet, ethernet, lldp
+from ryu.ofproto import ether
+
+class NetworkDiscovery(app_manager.RyuApp):
+    def __init__(self, *args, **kwargs):
+        super(NetworkDiscovery, self).__init__(*args, **kwargs)
+        self.topology_graph = nx.Graph()
+        self.monitor_thread = hub.spawn(self._monitor_topology)
+    
+    def _monitor_topology(self):
+        while True:
+            self._discover_topology()
+            hub.sleep(60)  # Monitoreo cada 10 segundos
+    
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        msg = ev.msg
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocol(ethernet.ethernet)
+
+        # if eth.ethertype == ether.ETH_TYPE_LLDP:
+        #     return  # Ignorar paquetes LLDP ya que se usan solo para descubrimiento
+
+        self.logger.info("Paquete recibido en switch %s", msg.datapath.id)
+
+    def _discover_topology(self):
+        self.topology_graph.clear()
+        switches = get_switch(self)
+        links = get_link(self)
+        hosts = get_host(self)
+        
+        # LOGS PARA DEPURACIÓN
+        # self.logger.info("Switches detectados: %s", [s.dp.id for s in switches])
+        self.logger.info("Switches detectados:")
+        for switch in switches:
+            mac_addresses = [port.hw_addr for port in switch.ports]
+            self.logger.info("  Switch %s - MACs: %s", switch.dp.id, mac_addresses)
+        
+        self.logger.info("Enlaces detectados: %s", [(l.src.dpid, l.dst.dpid) for l in links])
+        self.logger.info("Hosts detectados: %s", [h.mac for h in hosts])
+
+        for switch in switches:
+            self.topology_graph.add_node(switch.dp.id, type='switch')
+        
+        for link in links:
+            self.topology_graph.add_edge(link.src.dpid, link.dst.dpid, port=link.src.port_no)
+
+        for host in hosts:
+            self.topology_graph.add_node(host.mac, type='host')
+            self.topology_graph.add_edge(host.port.dpid, host.mac, port=host.port.port_no)
+        
+        self.logger.info("Topología descubierta: %s", self.topology_graph.edges)
+
+    @set_ev_cls(topo_event.EventSwitchEnter)
+    def switch_enter_handler(self, ev):
+        for switch in get_switch(self):
+            for port in switch.ports:
+                self.logger.info("Enviando LLDP a switch %s puerto %s", switch.dp.id, port.port_no)
+                datapath = switch.dp
+                ofproto = datapath.ofproto
+                parser = datapath.ofproto_parser
+                actions = [parser.OFPActionOutput(port.port_no)]
+                out = parser.OFPPacketOut(datapath=datapath, buffer_id=ofproto.OFP_NO_BUFFER,
+                                        in_port=ofproto.OFPP_CONTROLLER, actions=actions, data=None)
+                datapath.send_msg(out)
+
+        self.logger.info("Nuevo switch detectado: %s", ev.switch.dp.id)
+        self._discover_topology()
+
+    @set_ev_cls(topo_event.EventSwitchLeave)
+    def switch_leave_handler(self, ev):
+        self.logger.info("Switch eliminado: %s", ev.switch.dp.id)
+        self._discover_topology()
+    
+    @set_ev_cls(topo_event.EventLinkAdd)
+    def link_add_handler(self, ev):
+        self.logger.info("Nuevo enlace detectado: %s -> %s", ev.link.src.dpid, ev.link.dst.dpid)
+        self._discover_topology()
+
+    @set_ev_cls(topo_event.EventLinkDelete)
+    def link_delete_handler(self, ev):
+        self.logger.info("Enlace eliminado: %s -> %s", ev.link.src.dpid, ev.link.dst.dpid)
+        self._discover_topology()
+    
+    @set_ev_cls(topo_event.EventHostAdd)
+    def host_add_handler(self, ev):
+        self.logger.info("Nuevo host detectado: %s", ev.host.mac)
+        self._discover_topology()
---
+++ Scripts_Ryu/monitor.py
@@ -0,0 +1,137 @@
+# This is part of our final project for the Computer Networks Graduate Course at Georgia Tech
+# You can take the official course online too! Just google CS 6250 online at Georgia Tech.
+#
+# Contributors:
+#   
+#    Akshar Rawal (arawal@gatech.edu)
+#    Flavio Castro (castro.flaviojr@gmail.com)
+#    Logan Blyth (lblyth3@gatech.edu)
+#    Matthew Hicks (mhicks34@gatech.edu)
+#    Uy Nguyen (unguyen3@gatech.edu)
+#
+# To run:
+#    
+#    ryu-manager --observe-links shortestpath.py   
+#
+# Copyright (C) 2014, Georgia Institute of Technology.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+An OpenFlow 1.0 shortest path forwarding implementation.
+"""
+
+import logging
+import struct
+import networkx as nx
+
+from ryu.base import app_manager
+from ryu.controller import mac_to_port
+from ryu.controller import ofp_event
+from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_0
+from ryu.lib.mac import haddr_to_bin
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
+from ryu.topology.api import get_switch, get_link
+from ryu.app.wsgi import ControllerBase
+from ryu.topology import event, switches 
+
+
+class ProjectController(app_manager.RyuApp):
+    
+    OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectController, self).__init__(*args, **kwargs)
+        self.mac_to_port = {}
+        self.topology_api_app = self
+        self.net = nx.DiGraph()  # Directed graph to represent network topology
+        self.nodes = {}
+        self.links = {}
+        self.no_of_nodes = 0
+        self.no_of_links = 0
+        self.i = 0
+
+    # Handy function that lists all attributes in the given object
+    def ls(self, obj):
+        print("\n".join([x for x in dir(obj) if x[0] != "_"]))
+    
+    def add_flow(self, datapath, in_port, dst, actions):
+        ofproto = datapath.ofproto
+
+        match = datapath.ofproto_parser.OFPMatch(
+            in_port=in_port, dl_dst=haddr_to_bin(dst))
+
+        mod = datapath.ofproto_parser.OFPFlowMod(
+            datapath=datapath, match=match, cookie=0,
+            command=ofproto.OFPFC_ADD, idle_timeout=0, hard_timeout=0,
+            priority=ofproto.OFP_DEFAULT_PRIORITY,
+            flags=ofproto.OFPFF_SEND_FLOW_REM, actions=actions)
+        datapath.send_msg(mod)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocol(ethernet.ethernet)
+
+        dst = eth.dst
+        src = eth.src
+        dpid = datapath.id
+        self.mac_to_port.setdefault(dpid, {})
+
+        if src not in self.net:
+            self.net.add_node(src)
+            # self.net.add_edge(dpid, src, {'port': msg.in_port})
+            self.net.add_edge(dpid, src, port=msg.in_port)
+            self.net.add_edge(src, dpid)
+
+        if dst in self.net:
+            path = nx.shortest_path(self.net, src, dst)   
+            next = path[path.index(dpid) + 1]
+            out_port = self.net[dpid][next]['port']
+        else:
+            out_port = ofproto.OFPP_FLOOD
+
+        actions = [datapath.ofproto_parser.OFPActionOutput(out_port)]
+
+        # install a flow to avoid packet_in next time
+        if out_port != ofproto.OFPP_FLOOD:
+            self.add_flow(datapath, msg.in_port, dst, actions)
+
+        out = datapath.ofproto_parser.OFPPacketOut(
+            datapath=datapath, buffer_id=msg.buffer_id, in_port=msg.in_port,
+            actions=actions)
+        datapath.send_msg(out)
+    
+    @set_ev_cls(event.EventSwitchEnter)
+    def get_topology_data(self, ev):
+        switch_list = get_switch(self.topology_api_app, None)   
+        switches = [switch.dp.id for switch in switch_list]
+        self.net.add_nodes_from(switches)
+         
+        links_list = get_link(self.topology_api_app, None)
+        links = [(link.src.dpid, link.dst.dpid, {'port': link.src.port_no}) for link in links_list]
+        self.net.add_edges_from(links)
+        
+        links = [(link.dst.dpid, link.src.dpid, {'port': link.dst.port_no}) for link in links_list]
+        self.net.add_edges_from(links)
+        
+        print("********** List of links **********")
+        print(self.net.edges())
