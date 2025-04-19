bfcc3180da47d03720158f22c82801469fe82aba
AlejandroGarciaBautista <alu0101387604@ull.edu.es>
2025-04-19 19:44:35 +0000

    Enrutamiento con SDN completado - Falta testear más para descartar problemas

--- Scripts_mininet/topology.py
+++ Scripts_mininet/topology.py
@@ -12,8 +12,12 @@ def parse_arguments():
     parser.add_argument("--leaf", type=int, default=4, help="Cantidad de switches leaf (por defecto: 4)")
     parser.add_argument("--hosts", type=int, default=12, help="Cantidad de hosts por switch leaf (por defecto: 2)")
     parser.add_argument("--bw", type=int, default=1000, help="Capacidad de los enlaces Uplink en Mbps (por defecto: 1000 (máximo permitido))")
-    parser.add_argument("--rd", type=bool, default=True, help="Aplicar Redundancia en la red (por defecto: True)")
     parser.add_argument("-c", type=str, default="192.168.56.101", help="Dirección IP del controlador SDN (por defecto: 192.168.56.101)")
+
+    parser.add_argument("--rd", dest='rd', action='store_true', help="Aplicar Redundancia en la red")
+    parser.add_argument("--no-rd", dest='rd', action='store_false', help="No aplicar Redundancia en la red")
+
+    parser.set_defaults(rd=True)
 
     return parser.parse_args()
 
@@ -28,13 +32,13 @@ def create_spine_leaf_topology(spine_switches, leaf_switches, hosts_per_leaf, li
     leaves = [net.addSwitch(f"leaf{i+1}", dpid=f"{20000000000 + i+1:016x}", protocols="OpenFlow13") for i in range(leaf_switches)]
 
     uplinks = spine_switches
+    if redundancy: uplinks = uplinks * 2
     # Conectar switches leaf a los switches spine con redundancia
     for leaf in leaves:
         for spine in spines:
             net.addLink(leaf, spine, cls=TCLink, bw=link_bandwidth, htb=True)
             if redundancy: 
                 net.addLink(leaf, spine, cls=TCLink, bw=link_bandwidth, htb=True)
-                uplinks = uplinks * 2
 
     bw_leaf2host = (3 * (uplinks * link_bandwidth)) / hosts_per_leaf
     if bw_leaf2host > 1000: 
